db7b1c77c90bca032408647b6056f2c10cf29f50
agustinbalquin <agustinbalquin@gmail.com>
2016-05-14 17:14:57 -0700

    Updated Main, unpushed commits,rejecttimes,half do turn

--- exesrc/Main.cpp
+++ exesrc/Main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
+
 #include "Cutie.hpp"
 
 
 //#include "Main.hpp"
-
 
 enum Move {
 	ROCK,
@@ -19,99 +19,119 @@ enum SessionType {
 	INVALID
 };
 
+namespace {
+	SessionType choice() 
+	{
+		int res = 0;
+		
+		std::cout << "Would you like to create a game or join a game?\n";
+		std::cout << "1: Join\n2:Create\n\n ";
+		std::cin >> res;
+		
+		std::cin >> res;
+		
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		switch (res) {
+			case 1: 
+				return JOIN;
+			case 2:
+				return CREATE;
+			default:
+				return INVALID;
+		}
+	}
+
+	void getPlayerTurn(Move& temp)
+	{
+		int choice = 1;
+		std::cout << "Please enter your move!" << std::endl;
+		std::cout << "1: ROCK" << std::endl;
+		std::cout << "2: PAPER" << std::endl;
+		std::cout << "3: SCISSORS!" << std::endl;
+		std::cin >> choice;
+
+		if (choice == 1) {
+			temp = ROCK;
+		}
+		else if (choice == 2) {
+			temp =  PAPER;
+		}
+		else {
+			temp = SCISSORS;
+		}
+	}
+
+	void outputScore(int myScore, int theirScore)
+	{
+		std::cout << "YOU: " << myScore << std::endl;
+		std::cout << "OPPONENT: " << theirScore << std:: endl;
+	}
+
+	void updateScore(bool winner, int & myScore, int & theirScore) 
+	{
+		if (winner) {
+			myScore++;
+			return;
+		}
+		theirScore++;
+		return;
+	} 
+
+	bool compareTurn(Move myTurn, Move theirTurn)
+	{
+		bool winner = false;
+		if (myTurn == theirTurn) {
+			winner = false;				
+		}
+		else if (myTurn == ROCK) {
+			winner = (theirTurn == SCISSORS)? true : false;
+		}
+		else if (myTurn == PAPER) {
+			winner = (theirTurn == ROCK)? true : false;
+		}
+		else {
+			winner = (theirTurn == PAPER)? true : false;
+		}
+		return winner;
+	}
+
+	void outputEndgame(bool winner)
+	{
+
+		if (winner) {
+			std::cout << "Victory!" << std::endl;
+			std::cout << "YOU WON" << std::endl;
+		}
+		else {
+			std::cout << "Defeat!" << std::endl;
+			std::cout << "YOU LOST" << std::endl;
+		}
+	}
+
+	// char* turnConvert(Move turn) {
+	// 	if (turn == ROCK) {
+	// 		return new char(1);
+	// 	}
+	// 	else if (turn == PAPER) {
+	// 		return new char(2);
+	// 	}
+	// 	else {
+	// 		return new char(3);
+	// 	}
+	// }
+
+	bool checkInput(char* data) {
+		
+	}
+};
+
 // void getPlayerTurn(enum Move & temp);
 // void outputScore(int myScore, int theirScore);
 // void updateScore(bool winner, int & myScore, int & theirScore);
 
 // 
-SessionType choice() 
-{
-	int res = 0;
-	
-	std::cout << "Would you like to create a game or join a game?\n";
-	std::cout << "1: Join\n2:Create\n\n ";
-	std::cin >> res;
-	
-	std::cin >> res;
-	
-	std::cin.clear();
-	std::cin.ignore(256, '\n');
-	switch (res) {
-		case 1: 
-			return JOIN;
-		case 2:
-			return CREATE;
-		default:
-			return INVALID;
-	}
-}
 
-void getPlayerTurn(Move& temp)
-{
-	int choice = 1;
-	std::cout << "Please enter your move!" << std::endl;
-	std::cout << "1: ROCK" << std::endl;
-	std::cout << "2: PAPER" << std::endl;
-	std::cout << "3: SCISSORS!" << std::endl;
-	std::cin >> choice;
-
-	if (choice == 1) {
-		temp = ROCK;
-	}
-	else if (choice == 2) {
-		temp =  PAPER;
-	}
-	else {
-		temp = SCISSORS;
-	}
-}
-
-void outputScore(int myScore, int theirScore)
-{
-	std::cout << "YOU: " << myScore << std::endl;
-	std::cout << "OPPONENT: " << theirScore << std:: endl;
-}
-
-void updateScore(bool winner, int & myScore, int & theirScore) 
-{
-	if (winner) {
-		myScore++;
-		return;
-	}
-	theirScore++;
-	return;
-} 
-
-bool compareTurn(Move myTurn, Move theirTurn)
-{
-	bool winner = false;
-	if (myTurn == theirTurn) {
-		winner = false;				
-	}
-	else if (myTurn == ROCK) {
-		winner = (theirTurn == SCISSORS)? true : false;
-	}
-	else if (myTurn == PAPER) {
-		winner = (theirTurn == ROCK)? true : false;
-	}
-	else {
-		winner = (theirTurn == PAPER)? true : false;
-	}
-	return winner;
-}
-
-void outputEndgame(bool winner)
-{
-
-	if (winner) {
-		std::cout << "Victory!" << std::endl;
-		std::cout << "YOU WON" << std::endl;
-	}
-	else {
-		std::cout << "Defeat!" << std::endl;
-		std::cout << "YOU LOST" << std::endl;
-	}
-}
 
 int main(int argc, char *argv[])
 {
@@ -137,7 +157,8 @@ int main(int argc, char *argv[])
 	//cutie::connect();
 
 	//if(cutie::connect(address, timeout)) {
-
+		//ask user for reject input
+		cutie::setRejectTimes(5);
 		bool gameStarted = true;//gameInit(0,0);
 		bool gameActive = true;
 
@@ -148,6 +169,10 @@ int main(int argc, char *argv[])
 			Move myTurn = ROCK;
 			Move theirTurn = ROCK;
 
+			cutie::data Opponent;
+			cutie::data Player;
+			Opponent.length = 1;
+			Player.length = 1;
 
 			while (gameActive) {
 
@@ -155,15 +180,18 @@ int main(int argc, char *argv[])
 				outputScore(myScore, theirScore);
 				//Prompts User for turn
 				getPlayerTurn(myTurn);
+				Player.data = new char(static_cast<int>(myTurn));
 
-				//theirTurn =  cutie::doTurn(myTurn);
+				Opponent = cutie::doTurn(Player, );
+				theirTurn = static_cast<Move>(*(Opponent.data));
+
 				winner = compareTurn(myTurn,theirTurn);
 		
 				updateScore(winner, myScore, theirScore);
 
 
 				//Endgame check  Set final winner
-				if (myScore == 5 || theirScore == 5) {
+				if (myScore >= 5 || theirScore >= 5) {
 					gameActive = false;
 					if (myScore == 5) {
 						winner = true;
