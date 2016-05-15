16661c650f5eae13794e78aff0372db92b1f4670
Bradley Cai <bradcai24@gmail.com>
2016-05-15 00:12:52 +0000

    Added license to main.cpp

--- exesrc/Main.cpp
+++ exesrc/Main.cpp
@@ -1,13 +1,31 @@
+/*
+ * "Main.cpp"
+ *
+ * libcutie - Validate your opponent's data to detect cheating.
+ * Copyright (c) 2016 Ammon Smith, Auggie Balquin, Bradley Cai
+ * 
+ * libcutie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * libcutie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcutie.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
 #include <iostream>
 #include <string>
 #include <cerrno>
 #include <cstring>
 
 #include "Cutie.hpp"
-
-
 //#include "Main.hpp"
-
 
 enum Move {
 	ROCK,
